571b0b73a4d1a6ba0770c963c077232a96deaaf5
shinbunbun <takamuneyuto@gmail.com>
2022-06-09 17:57:06 +0900

    add write.sh

---
+++ write.sh
@@ -0,0 +1,5 @@
+arm-none-eabi-as -c startup.s -o startup.o
+arm-none-eabi-gcc -c main.c -o main.o -mcpu=cortex-m7 -Os
+arm-none-eabi-ld -T ldscript.ld main.o startup.o -o kernel.out
+arm-none-eabi-objcopy kernel.out -I ihex -O binary kernel.bin
+st-flash write kernel.bin 0x08000000
