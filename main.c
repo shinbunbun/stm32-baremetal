fcebbe5c42c4d7346abd6ca0d510ba2bcb46021e
shinbunbun <takamuneyuto@gmail.com>
2022-06-09 17:57:02 +0900

    fix main.c

--- main.c
+++ main.c
@@ -4,18 +4,18 @@
 #define mem(ADDR) *((volatile uint32_t *)(ADDR))
 
 #define GPIOB_PORT_BASE 0x40020400U
-#define GPIOB_MODER GPIOB_PORT_BASE + (uint32_t)0x00
-#define GPIOB_OTYPER GPIOB_PORT_BASE + (uint32_t)0x04
-#define GPIOB_PUPDR GPIOB_PORT_BASE + (uint32_t)0x0C
-#define GPIOB_ODR GPIOB_PORT_BASE + (uint32_t)0x14
+#define GPIOB_MODER (GPIOB_PORT_BASE + (uint32_t)0x00)
+#define GPIOB_OTYPER (GPIOB_PORT_BASE + (uint32_t)0x04)
+#define GPIOB_PUPDR (GPIOB_PORT_BASE + (uint32_t)0x0C)
+#define GPIOB_ODR (GPIOB_PORT_BASE + (uint32_t)0x14)
 
-#define RCC_BASE 0x40021000U
-#define RCC_AHBENR RCC_BASE + (uint32_t)0x14
+#define RCC_BASE 0x40023800U
+#define RCC_AHBENR RCC_BASE + (uint32_t)0x30
 
 int main(void)
 {
   mem(RCC_AHBENR) |= (1 << 1);
-  mem(GPIOB_MODER) = (0b01 << 2 * 3); // Set '01' to GPIOB port 3
+  mem(GPIOB_MODER) = (0b01 << 2 * 7); // Set '01' to GPIOB port 7
   mem(GPIOB_OTYPER) = 0x0000;         // All-push pull
   mem(GPIOB_PUPDR) = 0x000000000;     // No pullup-pulldown
 
